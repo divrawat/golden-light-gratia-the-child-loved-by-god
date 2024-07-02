40509846d2aa68a6d1f5c19628edfacf0fd835d2
divrawat <divrawat2001@gmail>
2024-07-02 22:36:42 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,208 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.goldenlightgratiathechildlovedbygod.in.net';
+
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Golden Light Gratia The Child Loved by God</u></a> is a heartwarming tale that follows the life of Gratia, a young girl blessed with divine favor. Set in a picturesque village, the story explores Gratia's journey as she navigates challenges with the unwavering support of her community and the guidance of her faith.`;
+
+export const MANGA_NAME = 'Golden Light Gratia The Child Loved by God';
+export const MANGA_AUTHOR = 'Not Known';
+export const MANGA_RELEASE = '2023';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Not Known';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Drama, Fantasy, Romance, Mahwa';
+
+export const CHAPTER_PREFIX = 'golden-light-gratia-the-child-loved-by-god-chapter';
+export const IMAGE_PREFIX = 'images/golden-light-gratia-the-child-loved-by-god';
+export const NEXT_PREVIOUS_PREFIX = 'manga/golden-light-gratia-the-child-loved-by-god-chapter';
+export const URL_PREFIX = 'manga/golden-light-gratia-the-child-loved-by-god-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/golden-light-gratia-the-child-loved-by-god-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://piedpiperfansub.me/wp-content/uploads/2024/01/gratia-1200x630.webp";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] rounded-full w-[75px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Golden Light Gratia The Child Loved by God';
+export const NavbarName = "GLGTCLBG";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. The story explores Gratia's journey as she navigates challenges with the unwavering support of her community and the guidance of her faith. Through moments of joy and hardship, Gratia's resilience and faith in God's love shine brightly, inspiring all who cross her path. This narrative beautifully illustrates the power of faith, community, and the enduring grace that surrounds a child touched by divine affection.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "Golden Light Gratia: The Child Loved by God unfolds in a serene village nestled amidst rolling hills and shimmering lakes, where the air is infused with a sense of peace and divine presence. At the heart of this tranquil setting is Gratia, a young girl whose golden hair seems to radiate with an ethereal glow, earning her the endearing nickname Golden Light Gratia."
+    },
+    {
+        id: 2,
+        content: "From an early age, Gratia is recognized not only for her striking appearance but also for her gentle spirit and unwavering faith.Born into a family deeply rooted in their faith, Gratia learns about God's love and grace through stories told by her grandmother, whose wisdom and kindness shape Gratia's worldview.As Gratia grows, her connection with God strengthens, and she becomes a source of comfort and inspiration to those around her."
+    },
+    {
+        id: 3,
+        content: "The village where Gratia resides is a close - knit community where everyone knows each other, and bonds are forged through shared joys and sorrows.Despite the idyllic surroundings, challenges occasionally arise: a harsh winter threatens crops, illness touches loved ones, and conflicts test relationships.Through it all, Gratia remains a beacon of hope, embodying the principles of kindness, forgiveness, and compassion."
+    },
+    {
+        id: 4,
+        content: "One particularly poignant moment in Gratia's journey occurs when a neighboring village suffers a devastating flood. Without hesitation, Gratia and her family mobilize the community to provide food, shelter, and comfort to the displaced families. In doing so, Gratia demonstrates the transformative power of empathy and solidarity, illustrating how faith can manifest in acts of love and service."
+    },
+    {
+        id: 5,
+        content: "As Gratia matures, her faith continues to deepen, guiding her through personal challenges and moments of doubt.Her unwavering belief in God's plan sustains her through difficult times, inspiring those around her to persevere with hope and resilience."
+    },
+    {
+        id: 6,
+        content: "Golden Light Gratia: The Child Loved by God is a testament to the enduring power of faith and community. It celebrates the simple yet profound moments of grace that shape our lives and reminds us of the profound impact one person's faith and kindness can have on an entire community. Through Gratia's journey, we are reminded that God's love shines brightly in the hearts of those who embrace it with humility and grace."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "0",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "1",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 14
+    }
+]
